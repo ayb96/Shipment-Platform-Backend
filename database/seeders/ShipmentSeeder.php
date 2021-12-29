db8c57a68cb700fc970869f244eca3a1f3955300
ayb96 <hussein-ayoub-96@hotmail.com>
2021-12-29 18:25:13 +0200

    some update

--- database/seeders/ShipmentSeeder.php
+++ database/seeders/ShipmentSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
+use App\Models\Shipment;
 class ShipmentSeeder extends Seeder
 {
     /**
@@ -11,19 +12,34 @@ class ShipmentSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    // public function run()
+    // {
+        // $faker = \Faker\Factory::create();
+        // \App\Models\User::all()->each(function ($user) use ($faker){
+        //     foreach (range(1, 30) as $i){
+        //         \App\Models\Shipment::create([
+        //             'address'=>$faker->address,
+        //             'waybill'=>$faker->sentence,
+        //             'name'=>$faker->name,
+        //             'phone'=>$faker->phoneNumber,
+        //             'user_id'=>$user->getKey(),
+        //         ]);
+        //     }
+        // });
+        public function run()
     {
-        $faker = \Faker\Factory::create();
-        \App\Models\User::all()->each(function ($user) use ($faker){
-            foreach (range(1, 30) as $i){
-                \App\Models\Shipment::create([
-                    'address'=>$faker->address,
-                    'waybill'=>$faker->sentence,
-                    'name'=>$faker->name,
-                    'phone'=>$faker->phoneNumber,
+        User::all()->each(
+           function($user){ for($i=0;$i<=60;$i++){
+                Shipment::create([
+                    'address'=>'address'.$i,
+                    'waybill'=>'waybill'.$i,
+                    'name'=>"User".$i,
+                    'phone'=>"7071727".$i,
                     'user_id'=>$user->getKey(),
+                   
                 ]);
-            }
-        });
+            }}
+        );
     }
+    
 }
